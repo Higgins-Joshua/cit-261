4d7c1bdab68c54715c3a9ff53867a3f373c1ae32
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-02-22 20:32:45 -0700

    finished 3,4,5,6 topics

--- web/topic3-4-5-6/js/apiHandler.js
+++ web/topic3-4-5-6/js/apiHandler.js
@@ -1,31 +1,34 @@
+var repos = "";
+
 document.getElementsByClassName('searchUser')[0].onclick = function() {
     var userName = document.getElementsByClassName('githubUsername')[0].value;
 
     if(userName == "Higgins723" || userName == "higgins723"){
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var data = JSON.parse(this.responseText);
-                transformData(data);
-            }
-        };
-        xhttp.open("GET", "/topic3-4-5-6/txt/higgins723.txt", true);
-        xhttp.send();
-
-        var newXhttp = new XMLHttpRequest();
-        newXhttp.onreadystatechange = function() {
+        var repoHttp = new XMLHttpRequest();
+        repoHttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 var repoData = JSON.parse(this.responseText);
                 transformRepoData(repoData);
             }
-        }
-        newXhttp.open("GET", "/topic3-4-5-6/txt/higgins723Repos.txt", true);
-        newXhttp.send();
+        };
+        repoHttp.open("GET", "/topic3-4-5-6/txt/higgins723Repos.txt", true);
+        repoHttp.send();
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText);
+                transformData(data, repos);
+            }
+        };
+        xhttp.open("GET", "/topic3-4-5-6/txt/higgins723.txt", true);
+        xhttp.send();
     }
 }
 
-function transformData(d) {
+function transformData(d, r) {
     var div = document.createElement('div');
+
     div.className = "row";
     div.innerHTML = `
         <div class="col-6">
@@ -35,16 +38,24 @@ function transformData(d) {
             <p><b>Username:</b> ${d.login}</p>
             <p><b>Github URL:</b> <a href="${d.html_url}" target="_blank">Link to Github</a></p>
             <p><b>Name:</b> ${d.name}</p>
-            <p>
-                <span><b>Repositories:</b></span>
-            </P>
+            <p><b>Repositories:</b><p>
+            <ul>
+                `
+                +
+                repos
+                +
+                `
+            </ul>
         </div>
     `;
+    repos = "";
     document.getElementsByClassName('gitContent')[0].appendChild(div);
 }
 
 function transformRepoData(d) {
-    console.log(d);
+    for (var i in d) {
+        repos += `<li><a target="_blank" href="${d[i].html_url}">${d[i].name}</a></li>`;
+    }
 }
 
 function changeButtonStatus() {
